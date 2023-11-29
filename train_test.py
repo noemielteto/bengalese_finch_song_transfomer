a63c9b7cf5042c4385bde51946228767172e04c5
Tankred Saanum <62603648+tankred-saanum@users.noreply.github.com>
2023-11-29 14:27:36 +0100

    Update train_test.py

--- train_test.py
+++ train_test.py
@@ -17,24 +17,28 @@ train_idx = torch.randperm(N)[:int(N*0.6)]
 test_idx = torch.randperm(N)[int(N*0.6):]
 
 all_songs_tokens_flattened = torch.cat([data[i][0] for i in range(len(data))])
-n_tokens = all_songs_tokens_flattened.argmax() + 1  # +1 since indexing is from 0
-max_length = 25
+#all_songs_tokens_flattened.argmax()
+n_tokens = all_songs_tokens_flattened.max() + 1  # +1 since indexing is from 0
+max_length = 50
 
-model = Songformer(ntokens=n_tokens, n_heads=8, num_layers=2, context_length=max_length)
+model = Songformer(ntokens=n_tokens, n_heads=50, num_layers=3, context_length=max_length, latent_dims=100)
 
-optimizer = optim.Adam(model.parameters(), lr=0.0003)
 
-n_iters = 250
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
+
+n_iters = 1000
 batch_size = 50
 losses = []
 for i in range(n_iters):
-    idx = torch.randint(len(train_idx), (batch_size, ))
+    idx_ = torch.randint(len(train_idx), (batch_size, ))
+    idx = train_idx[idx_]
     songs = [data[song_idx.item()] for song_idx in idx]
 
     # song[0] is the token tensor; song[1] is the duration tensor
 
     min_song_length = min([len(song[0]) for song in songs])
     max_length_in_iter = min(max_length, min_song_length) - 1  # minus one because the sequence has to contain one more element to be predicted
+    max_length_in_iter = torch.randint(1, max_length_in_iter, (1, )).item()
     tokens      = []
     next_tokens = []
     durations   = []
@@ -58,7 +62,63 @@ for i in range(n_iters):
     print(loss.item())
     losses.append(loss.item())
 
+
+context_window = 3
+idx = torch.randint(len(train_idx), (10, ))
+eval_songs = [data[song_idx.item()][0][:context_window] for song_idx in idx]
+context = torch.stack(eval_songs).permute(1, 0)
+
+
+synthetic_songs = model.generate_autoregressive(context, n_steps=15, deterministic=False)
+mappings = torch.load(f'data/{bird}_syllable_mapping.pt')
+synthetic_songs[:, 0]
+def tensor2song(x):
+    songs = []
+    for i in range(x.size(1)):
+        arr = x[:, i]
+        idx_list = arr.tolist()
+        syll_list = [mappings['idx2syl'][idx] for idx in idx_list]
+        songs.append(syll_list)
+    return songs
+
+song_list = tensor2song(synthetic_songs)
+
+for song in song_list:
+    print(song)
+
+
+
+eval_songs = [data[song_idx.item()][0][:context_window+15] for song_idx in idx]
+context = torch.stack(eval_songs).permute(1, 0)
+context
+song_list = tensor2song(context)
+
+for song in song_list:
+    print(song)
+
+
+
+
+
+def evaluate(context_window=25, batch_size=250):
+    
+    idx_ = torch.randint(len(test_idx), (batch_size, ))
+    idx = test_idx[idx_]
+    eval_contexts = [data[song_idx.item()][0][:context_window] for song_idx in idx]
+    eval_targets = [data[song_idx.item()][0][context_window] for song_idx in idx]
+    context = torch.stack(eval_contexts).permute(1, 0)
+    tgt = torch.stack(eval_targets)#.permute(1, 0)
+    embedding = model(context)
+    preds = model.predict_next(embedding[-1, :])
+    diff = preds - tgt
+    idx = diff == 0
+    acc = idx.sum()/len(idx)
+    return acc.item()
+
+for i in range(1, 18):
+
+    accuracy = evaluate(context_window=i)
+    print('context size: ', i, ' Accuracy: ', accuracy*100, '%')
+
+
 # Evaluation
-x = torch.tensor([1,1,1])
-z = model.forward(x)
-h = model.predict(z)
